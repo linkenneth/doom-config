19aff1546cd6a7afafe9a24a93fcc689b10707f1
Kenneth Lin <klin@liftoff.io>
2023-09-13 20:08:52 -0700

    bump emacs and add black-isort formatter

--- modules/python.el
+++ modules/python.el
@@ -3,17 +3,24 @@
 (after! python
   ;; make def block continuations indent as expected
   ;; https://github.com/hlissner/doom-emacs/issues/3028
-  (setq python-indent-def-block-scale 1))
+  (setq python-indent-def-block-scale 1)
 
+  ;; use 88 characters line length when using Black
+  (add-hook 'python-mode-hook
+            (lambda () (when (liftoff-project-p (doom-project-root))
+                         (set-fill-column 88)))))
+
+;; HACK: if Doom supports multi-formatters better, change this to use
+;; a single set-formatter! call
 (set-formatter!
-  'liftoff-black
+  'liftoff-black-isort
   (list
-   (concat (getenv "LIFTOFF_VENV_ROOT") "/pytool-pydev/bin/black")
-   "-q" ;; quiet
-   "-")
+   (concat (getenv "HOME") "/bin/black-isort"))
   :modes
   '(python-mode (liftoff-project-p (doom-project-root))))
 
-;; TODO: isort and black on save
-
-;; TODO: add mypi (and liftoff mypi) support
+;; TODO: add mypi (and liftoff mypi) support (we already have some?)
+;; TODO: g a ' text object for triple quotes doesn't copy the whole triple
+;; quote, only the first inner quote
+;; TODO: triple quote with formatted string doesn't display proper syntax
+;; un-highlighting for the formatted variables
--- modules/sql.el
+++ modules/sql.el
@@ -1,7 +1,11 @@
 ;;; sql.el -*- lexical-binding: t; -*-
 
+;; TODO: sqlfluff is very slow. be on the lookout for either speed improvements
+;; or something better
+;; TODO: also, sqlfluff has an open issue to support trino dialect. keep an eye
+;; out for this and use when available
 (set-formatter!
   'sqlfluff-postgres
   (concat (getenv "HOME") "/.doom.d/bin/sqlfluff-postgres")
   :modes
-  'sql-mode)
+  '(sql-mode))
